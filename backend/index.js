96f5c4629358d20b633a2fe5878b373429f909f3
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-07-31 14:02:01 +0900

    init

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT ?? 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
